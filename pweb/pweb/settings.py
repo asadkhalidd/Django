eedd593d18cbc0113b421f1013101c39f4f6f57d
asadkhalidd <asadkhalid421@gmail.com>
2022-02-23 18:14:40 +0500

    pweb

--- pweb/pweb/settings.py
+++ pweb/pweb/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'pweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'userdb',
+        'USER'    : 'root',                     
+        'PASSWORD': 'admin',              
+        'HOST'    : 'localhost',                
+        'PORT'    : '3306',
     }
 }
 
